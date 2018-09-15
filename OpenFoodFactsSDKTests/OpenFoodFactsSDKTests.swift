5b8e0ea5e391b1a25be087f7834d208a5873a0c1
kegham <kegham1983@gmail.com>
2018-09-15 23:53:17 +0200

    Tests added

--- OpenFoodFactsSDKTests/OpenFoodFactsSDKTests.swift
+++ OpenFoodFactsSDKTests/OpenFoodFactsSDKTests.swift
@@ -12,7 +12,6 @@ class OpenFoodFactsSDKTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        //tests
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
